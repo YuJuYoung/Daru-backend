667dbb4c082d765eb23c7a74c57f1f74c164c89b
유주영 <qlenfrl0577@naver.com>
2024-05-03 22:09:14 +0900

    WebConfig - PasswordEncoder 추가

--- daru/src/main/java/com/pizeon/daru/config/WebConfig.java
+++ daru/src/main/java/com/pizeon/daru/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.pizeon.daru.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
@@ -13,6 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
 				.allowedOriginPatterns("*")
 				.allowedMethods("POST")
 				.allowedHeaders("Content-Type");
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 }
